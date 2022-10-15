8369d8cdb4cd25921bd4609702be9870cc0763f5
anoop ks <anoopsivan8@gmail.com>
2022-10-15 14:14:42 +0530

    temp home.html created
    connected to the booking app

--- Hotel/booking/admin.py
+++ Hotel/booking/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from booking.models import Hotel
 
 # Register your models here.
+
+class Booking(admin.ModelAdmin):
+    list_display= ('name','location','rating','description','type')
+
+
+admin.site.register(Hotel,Booking)
+
+
--- Hotel/booking/models.py
+++ Hotel/booking/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Hotel(models.Model):
+    name = models.CharField(max_length=30)
+    description= models.CharField(max_length=100)
+    location= models.CharField(max_length=20)
+    type = models.CharField(max_length = 30)
+    rating= models.CharField(max_length=10)
